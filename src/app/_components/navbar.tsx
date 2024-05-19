c34457318824377ecc3c725fd1b68d2d4ebb4a89
Michael G. Padin <padinmichael201@gmail.com>
2024-05-19 18:43:12 +0800

    add navbar for non-authenticated users

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,39 @@
+"use client"
+import Link from "next/link"
+
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+
+const navItems = [
+  { name: "Become a farmer", link: "signup" },
+  { name: "Sign in", link: "login" },
+  { name: "Join", link: "signup" },
+]
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="mt-4 hidden flex-col items-center rounded-lg border p-4  font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 lg:flex rtl:space-x-reverse">
+        {navItems.map((item, idx) => (
+          <li key={idx}>
+            <Link
+              href={`/${item.link}`}
+              className={`${
+                item.name === "Join"
+                  ? cn(
+                      buttonVariants({ variant: "outline" }),
+                      "border-primary text-primary outline-primary hover:bg-transparent hover:text-primary",
+                    )
+                  : ""
+              } `}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
