7d7539e60e08dc0bda6c00ff34f98de3225b167e
Michael G. Padin <padinmichael201@gmail.com>
2024-05-20 18:45:34 +0800

    update login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -15,13 +15,16 @@ export default async function LoginPage() {
     <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:min-h-screen">
       <div className="relative hidden h-full flex-col bg-muted p-10  dark:border-r lg:flex">
         <div className="absolute inset-0 ">
-          <Image src="login.svg" alt="login image" objectFit="contain" fill />
+          <Image
+            src="/placeholder.svg"
+            alt="login image"
+            objectFit="cover"
+            fill
+          />
         </div>
         <div className="relative z-20 flex items-center text-lg font-medium">
-          <h1 className="text-2xl font-black">
-            <Link href={"/"}>
-              <span className="text-primary">Share</span>Surplus
-            </Link>
+          <h1 className="text-2xl font-black text-primary">
+            <Link href="/">Farm2go</Link>
           </h1>
         </div>
         {/* <div className="relative z-20 mt-auto">
