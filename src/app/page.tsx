872a617706701b448798892fab2bb6abd28cabfd
Michael G. Padin <padinmichael201@gmail.com>
2024-05-29 05:41:58 +0800

    feat: update root page as landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,115 @@
-import Image from "next/image"
+import { Apple, Banana, Carrot, Vegan } from "lucide-react"
+import Link from "next/link"
 
-export default function Home() {
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { categories, products } from "@/data"
+import { cn } from "@/lib/utils"
+
+import CategoryCard from "./_components/category-card"
+import Footer from "./_components/footer"
+import Navbar from "./_components/navbar"
+import ProductCard from "./_components/product-card"
+
+const Home = async () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <header className="fixed inset-x-0 top-0 z-10 border-b bg-white">
+        <div className="container mx-auto flex h-20 items-center justify-between px-4 ">
+          <Link
+            className="flex items-center gap-2 text-3xl font-black text-[#404145]"
+            href="/"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <img src="/logo.svg" alt="" className="h-[50px] w-[50px]" />
+            Farm2go
+          </Link>
+          <Navbar />
         </div>
-      </div>
+      </header>
+      <main className="">
+        <section className=" m-auto w-full pb-6 pt-24 ">
+          {/* <div className="absolute  inset-0  bg-black/20 backdrop-blur-sm"></div> */}
+          <div className="container relative  mx-auto h-full rounded-lg bg-primary  px-4 md:px-6">
+            <div className="my-4 flex items-center  justify-center py-20">
+              <Vegan
+                className="absolute -bottom-5 left-10 h-32 w-32 stroke-white"
+                strokeWidth={1}
+              />
+              <Carrot
+                className="absolute -bottom-5 right-10 h-32 w-32 stroke-white"
+                strokeWidth={1}
+              />
+              <Banana
+                className="absolute -bottom-5 right-60 h-32 w-32 stroke-white"
+                strokeWidth={1}
+              />
+              <Apple
+                className="absolute -bottom-5 left-60 h-32 w-32 stroke-white"
+                strokeWidth={1}
+              />
 
-      <div className="before:bg-gradient-radial after:bg-gradient-conic relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <div className="relative space-y-10 text-center text-white">
+                <h1 className="text-3xl font-bold leading-normal tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                  Discover the Best Local Produce
+                </h1>
+                <p className="mx-auto max-w-[700px] md:text-xl">
+                  Connect directly with farmers and get the freshest,
+                  highest-quality produce delivered to your door.
+                </p>
+                <div className="m-auto flex w-2/3 justify-center gap-2 rounded-lg bg-white p-2">
+                  <Input
+                    placeholder="Search products"
+                    className={`${cn("border-none text-black focus-visible:ring-transparent")}`}
+                  />
+                  <Button>Search</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section className="container mx-auto px-4">
+          <div className="flex  justify-between">
+            <h2 className="mb-4 text-2xl font-bold">Featured Products</h2>
+            <Link
+              href="/products"
+              className="leading-normal text-primary underline-offset-2 hover:underline"
+            >
+              See all
+            </Link>
+          </div>
+          <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {products.map((product, index) => (
+              <ProductCard
+                description={product.description}
+                key={index}
+                images={product.images}
+                title={product.title}
+                price={product.price}
+                farmer={product.farm}
+                unit={product.unit}
+              />
+            ))}
+          </div>
+        </section>
+        <section className="container mx-auto mt-8 px-4">
+          <h2 className="mb-4 text-2xl font-bold">Browse by Category</h2>
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {categories.map((category, index) => (
+              <CategoryCard
+                key={index}
+                image={category.image}
+                title={category.title}
+                description={category.description}
+                link={category.link}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
   )
 }
+
+export default Home
