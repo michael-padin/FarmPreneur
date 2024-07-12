f065fb07156116e04a82b532ba80a091ba9e673b
Michael G. Padin <padinmichael201@gmail.com>
2024-07-12 10:04:20 +0800

    fix: update signup page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 import Image from "next/image"
 import Link from "next/link"
 
+import GoogleButton from "@/app/_components/google-button"
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 
 import SignUpFormTabs from "./_components/form-tabs"
@@ -15,8 +16,52 @@ export const metadata: Metadata = {
 export default async function SignupPage() {
 	return (
 		<div className="h-screen w-full overflow-hidden lg:grid lg:grid-cols-2 xl:min-h-screen">
-			<div className="relative hidden h-full flex-col bg-muted p-10  dark:border-r lg:flex">
-				<div className="absolute inset-0 ">
+			<ScrollArea className="h-screen">
+				<div className="flex items-center justify-center p-4">
+					<div className="mx-auto grid gap-6">
+						<div className="grid gap-2 text-center">
+							<Link
+								className="flex items-center justify-center gap-2 text-3xl font-black text-[#404145] lg:hidden"
+								href="/"
+							>
+								<img src="/logo.svg" alt="" className="h-[100px] w-[100px]" />
+							</Link>
+							<h1 className="text-3xl font-bold">Sign Up</h1>
+							<p className="text-balance text-muted-foreground">
+								Enter your information to create an account
+							</p>
+						</div>
+						{/* <SignUpFormTabs /> */}
+						<GoogleButton />
+						<div className="mt-4 text-center text-sm">
+							Already have an account?{" "}
+							<Link href="/login" className="underline">
+								Log in
+							</Link>
+						</div>
+						<p className="px-8 text-center text-sm text-muted-foreground">
+							By clicking continue, you agree to our{" "}
+							<Link
+								href="/terms"
+								className="underline underline-offset-4 hover:text-primary"
+							>
+								Terms of Service
+							</Link>{" "}
+							and{" "}
+							<Link
+								href="/privacy"
+								className="underline underline-offset-4 hover:text-primary"
+							>
+								Privacy Policy
+							</Link>
+							.
+						</p>
+					</div>
+				</div>
+				<ScrollBar />
+			</ScrollArea>
+			<div className="relative hidden h-full flex-col bg-muted p-10 dark:border-r lg:flex">
+				<div className="absolute inset-0">
 					<Image
 						src="/placeholder.svg"
 						alt="login image"
@@ -50,49 +95,6 @@ export default async function SignupPage() {
             </blockquote>
           </div> */}
 			</div>
-			<ScrollArea className="h-screen">
-				<div className="flex items-center justify-center p-4">
-					<div className="mx-auto grid gap-6">
-						<div className="grid gap-2 text-center">
-							<Link
-								className="flex items-center justify-center gap-2 text-3xl font-black text-[#404145] lg:hidden"
-								href="/"
-							>
-								<img src="/logo.svg" alt="" className="h-[100px] w-[100px]" />
-							</Link>
-							<h1 className="text-3xl font-bold">Sign Up</h1>
-							<p className="text-balance text-muted-foreground">
-								Enter your information to create an account
-							</p>
-						</div>
-						<SignUpFormTabs />
-						<div className="mt-4 text-center text-sm">
-							Already have an account?{" "}
-							<Link href="/login" className="underline">
-								Log in
-							</Link>
-						</div>
-						<p className="px-8 text-center text-sm text-muted-foreground">
-							By clicking continue, you agree to our{" "}
-							<Link
-								href="/terms"
-								className="underline underline-offset-4 hover:text-primary"
-							>
-								Terms of Service
-							</Link>{" "}
-							and{" "}
-							<Link
-								href="/privacy"
-								className="underline underline-offset-4 hover:text-primary"
-							>
-								Privacy Policy
-							</Link>
-							.
-						</p>
-					</div>
-				</div>
-				<ScrollBar />
-			</ScrollArea>
 		</div>
 	)
 }
