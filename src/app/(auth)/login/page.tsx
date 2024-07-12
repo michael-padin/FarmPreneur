268db77373b37b52a66cc484322bcd3bc4c1e00d
Michael G. Padin <padinmichael201@gmail.com>
2024-07-12 10:02:27 +0800

    fix: update login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from "next"
 import Image from "next/image"
 import Link from "next/link"
 
+import GoogleButton from "@/app/_components/google-button"
+
 import LoginForm from "./_components/login-form"
 
 export const metadata: Metadata = {
@@ -13,8 +15,32 @@ export const metadata: Metadata = {
 export default async function LoginPage() {
 	return (
 		<div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:min-h-screen">
-			<div className="relative hidden h-full flex-col bg-muted p-10  dark:border-r lg:flex">
-				<div className="absolute inset-0 ">
+			<div className="mx-auto flex items-center justify-center p-4">
+				<div className="mx-auto grid gap-6 md:w-[400px]">
+					<Link
+						className="flex items-center justify-center gap-2 text-3xl font-black text-[#404145] lg:hidden"
+						href="/"
+					>
+						<img src="/logo.svg" alt="" className="h-[100px] w-[100px]" />
+					</Link>
+					<div className="grid gap-2 text-center">
+						<h1 className="text-3xl font-bold">Login</h1>
+						<p className="text-balance text-muted-foreground">
+							Enter your credentials below to login to your account
+						</p>
+					</div>
+					{/* <LoginForm /> */}
+					<GoogleButton />
+					<div className="mt-4 text-center text-sm">
+						Don&apos;t have an account?{" "}
+						<Link href="/signup" className="underline">
+							Sign up
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className="relative hidden h-full flex-col bg-muted p-10 dark:border-r lg:flex">
+				<div className="absolute inset-0">
 					<Image
 						src="/placeholder.svg"
 						alt="login image"
@@ -37,29 +63,6 @@ export default async function LoginPage() {
               <footer className="text-sm">Sofia Davis</footer>
             </blockquote>
           </div> */}
-			</div>
-			<div className="mx-auto flex items-center justify-center p-4">
-				<div className="mx-auto grid gap-6 md:w-[400px] ">
-					<Link
-						className="flex items-center justify-center gap-2 text-3xl font-black text-[#404145] lg:hidden"
-						href="/"
-					>
-						<img src="/logo.svg" alt="" className="h-[100px] w-[100px]" />
-					</Link>
-					<div className="grid gap-2 text-center">
-						<h1 className="text-3xl font-bold">Login</h1>
-						<p className="text-balance text-muted-foreground">
-							Enter your credentials below to login to your account
-						</p>
-					</div>
-					<LoginForm />
-					<div className="mt-4 text-center text-sm">
-						Don&apos;t have an account?{" "}
-						<Link href="/signup" className="underline">
-							Sign up
-						</Link>
-					</div>
-				</div>
 			</div>
 		</div>
 	)
