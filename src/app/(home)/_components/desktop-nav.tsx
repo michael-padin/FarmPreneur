b57b1747f7917ca536519599496a7d154faef2fd
Michael G. Padin <padinmichael201@gmail.com>
2024-07-13 16:06:10 +0800

    feat: add desktop nav component

---
+++ src/app/(home)/_components/desktop-nav.tsx
@@ -0,0 +1,21 @@
+import { Navbar } from "@/app/_components"
+import Link from "next/link"
+
+const DesktopNav = () => {
+	return (
+		<header className="fixed inset-x-0 top-0 z-10 hidden border-b bg-white lg:block">
+			<div className="container mx-auto flex h-20 items-center justify-between px-4">
+				<Link
+					className="flex items-center gap-2 text-3xl font-black text-[#404145]"
+					href="/"
+				>
+					<img src="/logo.svg" alt="" className="h-[50px] w-[50px]" />
+					Farm2go
+				</Link>
+				<Navbar />
+			</div>
+		</header>
+	)
+}
+
+export default DesktopNav
