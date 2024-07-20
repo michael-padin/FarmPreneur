f064d1ac62ad9b56614479b8c288d3947d35351c
Michael G. Padin <padinmichael201@gmail.com>
2024-07-20 09:12:01 +0800

    feat: modified home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,73 @@
+import { Apple, Banana, Carrot, Vegan } from "lucide-react"
+import Link from "next/link"
+
+import { categories, products } from "@/data"
+import { cn } from "@/lib/utils"
+
+import { auth } from "@/auth"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import Footer from "../_components/footer"
+import {
+	BottomNav,
+	Categories,
+	CategoryCard,
+	DailyDiscovery,
+	DesktopNav,
+	Features,
+	MobileNav,
+	ProductCard
+} from "./_components"
+import { Navbar } from "../_components"
+
+const Home = async () => {
+	return (
+		<>
+			<header>
+				<MobileNav />
+				<DesktopNav />
+			</header>
+			<main className="bg-secondary pb-12">
+				<section className="m-auto w-full">
+					{/* <div className="absolute  inset-0  bg-black/20 backdrop-blur-sm"></div> */}
+					<div className="container relative mx-auto h-full px-0 lg:px-4 lg:pt-[101px]">
+						<div className="flex items-center justify-center bg-primary pb-20 pt-28 lg:rounded-lg lg:py-20">
+							{/* <Vegan
+								className="absolute -bottom-5 left-10 h-32 w-32 stroke-white"
+								strokeWidth={1}
+							/>
+							<Carrot
+								className="absolute -bottom-5 right-10 h-32 w-32 stroke-white"
+								strokeWidth={1}
+							/>
+							<Banana
+								className="absolute -bottom-5 right-60 h-32 w-32 stroke-white"
+								strokeWidth={1}
+							/>
+							<Apple
+								className="absolute -bottom-5 left-60 h-32 w-32 stroke-white"
+								strokeWidth={1}
+							/> */}
+
+							<div className="relative space-y-5 text-center text-white lg:space-y-10">
+								<h1 className="text-3xl font-bold leading-normal tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+									Discover the Best Local Produce
+								</h1>
+								<p className="mx-auto max-w-[700px] lg:text-xl">
+									Connect directly with farmers and get the freshest,
+									highest-quality produce delivered to your door.
+								</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<Categories />
+				<Features />
+				<DailyDiscovery />
+			</main>
+			{/* <Footer /> */}
+		</>
+	)
+}
+
+export default Home
