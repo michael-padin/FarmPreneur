ae81c6eb506a4caa17e15505585f710f2ea18a3c
Michael Padin <padinmichael201@gmail.com>
2024-10-03 06:46:01 +0800

    feat: update dashboard layout to check server session

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
 import React from "react"
 
 interface DashboardLayoutProps {
@@ -5,8 +7,14 @@ interface DashboardLayoutProps {
 	admin: React.ReactNode
 }
 
-const DashboardLayout = ({ farmer, admin }: DashboardLayoutProps) => {
-	const role = "FARMER"
+const DashboardLayout = async ({ farmer, admin }: DashboardLayoutProps) => {
+	const session = await auth()
+
+	if (!session) {
+		redirect("/login")
+	}
+
+	const role = session?.user.role
 
 	return <div>{role === "FARMER" ? farmer : admin}</div>
 }
