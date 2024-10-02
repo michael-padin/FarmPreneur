6ca25b74654b06223de48c4cde81f2c30e544d4e
Michael Padin <padinmichael201@gmail.com>
2024-10-03 06:57:04 +0800

    fix: description meta data comment for now

--- src/app/dashboard/@farmer/products/page.tsx
+++ src/app/dashboard/@farmer/products/page.tsx
@@ -32,8 +32,8 @@ import {
 } from "@/components/ui/table"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-export const description =
-	"An products dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. It displays a list of products in a table with actions."
+// export const description =
+// 	"An products dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. It displays a list of products in a table with actions."
 
 export default function ProductsPage() {
 	return (
