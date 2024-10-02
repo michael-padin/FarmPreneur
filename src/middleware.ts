b9cc00d1963e698f6b35f412561e67d7862df28a
Michael Padin <padinmichael201@gmail.com>
2024-10-02 19:25:51 +0800

    fix: update middleware to redirect farmers on dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -35,10 +35,10 @@ export default auth((req): Response | void | Promise<Response | void> => {
 	 */
 	if (isAuthRoute) {
 		if (isLoggedIn) {
-			if (req.auth?.user.isProfileComplete) {
-				return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+			if (req.auth?.user.role === "FARMER") {
+				return NextResponse.redirect(new URL("/dashboard", nextUrl))
 			}
-			return NextResponse.redirect(new URL("/complete-profile", nextUrl))
+			return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
 		}
 		return
 	}
