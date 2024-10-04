1003a7a0fded87819e94555fcc518fa932b049de
Michael Padin <padinmichael201@gmail.com>
2024-10-04 13:43:39 +0800

    feat: add products data access

---
+++ src/data-access/products.ts
@@ -0,0 +1,17 @@
+import { createProductType } from "@/app/dashboard/@farmer/products/create/types"
+import { db } from "@/lib/db"
+
+export const createProduct = async (data: createProductType) => {
+	await db.product.create({
+		data: {
+			farmerId: data.farmerId,
+			title: data.title,
+			description: data.description,
+			category: data.category,
+			price: data.price,
+			location: data.location,
+			images: data.images,
+			quantity: data.quantity
+		}
+	})
+}
