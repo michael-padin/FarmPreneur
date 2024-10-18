1c7634049e697e9b3c058cce0298b6dac6bbe447
Michael Padin <padinmichael201@gmail.com>
2024-10-19 01:41:14 +0800

    feat: add copy-to-clipboard component

---
+++ src/app/dashboard/_components/copy-to-clipboard.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button"
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger
+} from "@/components/ui/tooltip"
+import { Check, Copy } from "lucide-react"
+import { useState } from "react"
+
+const CopyToClipboard = ({ value }: { value: string }) => {
+	const [isCopied, setIsCopied] = useState(false)
+
+	const copyToClipboard = async () => {
+		await navigator.clipboard.writeText(value)
+		setIsCopied(true)
+		setTimeout(() => setIsCopied(false), 2000)
+	}
+
+	return (
+		<Tooltip>
+			<TooltipTrigger asChild>
+				<Button
+					variant="ghost"
+					size="icon"
+					className="h-5 w-5 p-0"
+					onClick={copyToClipboard}
+				>
+					{isCopied ? (
+						<Check className="h-4 w-4 text-green-500" />
+					) : (
+						<Copy className="h-4 w-4" />
+					)}
+					<span className="sr-only">Copy ID</span>
+				</Button>
+			</TooltipTrigger>
+			<TooltipContent>
+				<p>{isCopied ? "Copied!" : value}</p>
+			</TooltipContent>
+		</Tooltip>
+	)
+}
+
+export default CopyToClipboard
