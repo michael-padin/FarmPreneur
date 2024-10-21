cb61709f4c16ab695548999bf1c60d66873b2063
Michael Padin <padinmichael201@gmail.com>
2024-10-22 01:55:42 +0800

    types: add types SearchParams, Option, DataTableFilterField, DataTableFilterOption

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,31 @@ export const LoginSchema = z.object({
 	password: z.string().min(1)
 })
 export type LoginType = z.infer<typeof LoginSchema>
+
+export interface SearchParams {
+	[key: string]: string | string[] | undefined
+}
+
+export interface Option {
+	label: string
+	value: string
+	icon?: React.ComponentType<{ className?: string }>
+	withCount?: boolean
+}
+
+export interface DataTableFilterField<TData> {
+	label: string
+	value: keyof TData
+	placeholder?: string
+	options?: Option[]
+}
+
+export interface DataTableFilterOption<TData> {
+	id: string
+	label: string
+	value: keyof TData
+	options: Option[]
+	filterValues?: string[]
+	filterOperator?: string
+	isMulti?: boolean
+}
