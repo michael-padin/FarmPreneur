57cd5b716df94c6111b15ca3779676a20cb42697
Michael Padin <padinmichael201@gmail.com>
2024-10-21 11:47:03 +0800

    feat: update users page to use db data

--- src/app/dashboard/@admin/users/page.tsx
+++ src/app/dashboard/@admin/users/page.tsx
@@ -1,32 +1,20 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-
 import { DataTable } from "./_components/data-table"
 import { columns } from "./_components/columns"
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
+import { getUsersUseCase } from "@/use-cases/users"
 
-// Mock data for demonstration
-const users = Array.from({ length: 1000 }, (_, i) => ({
-	id: `${i + 1}`,
-	name: `User ${i + 1}`,
-	email: `user${i + 1}@example.com`,
-	role: ["FARMER", "BUYER", "ADMIN"][Math.floor(Math.random() * 3)],
-	isSellerApproved: Math.random() > 0.5,
-	isVerified: Math.random() > 0.3,
-	createdAt: new Date(
-		Date.now() - Math.floor(Math.random() * 10000000000)
-	).toISOString()
-}))
+const UsersPage = async () => {
+	const session = await auth()
+	if (!session || session.user.role !== "ADMIN") {
+		redirect("/login")
+	}
 
-const UsersPage = () => {
+	const users = await getUsersUseCase()
+
 	return (
 		<div className="p-1 lg:p-4">
-			<Card className="">
-				<CardHeader>
-					<CardTitle>User Management</CardTitle>
-				</CardHeader>
-				<CardContent className="">
-					<DataTable columns={columns} data={users} />
-				</CardContent>
-			</Card>
+			<DataTable columns={columns} data={users} />
 		</div>
 	)
 }
