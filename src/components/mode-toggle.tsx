140eead639df163a96d66bf0d5f2a3f6d273ff0c
Michael Padin <padinmichael201@gmail.com>
2024-10-22 20:41:41 +0800

    feat: update theme toggler

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import { Moon, Sun } from "lucide-react"
+import { LaptopIcon, MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
-import * as React from "react"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -12,30 +11,32 @@ import {
 	DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu"
 
-const ModeToggle = () => {
+export function ModeToggle() {
 	const { setTheme } = useTheme()
+
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="ghost" size="icon">
-					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+				<Button variant="ghost" size="icon" className="size-8">
+					<SunIcon className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+					<MoonIcon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 					<span className="sr-only">Toggle theme</span>
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
 				<DropdownMenuItem onClick={() => setTheme("light")}>
-					Light
+					<SunIcon className="mr-2 size-4" />
+					<span>Light</span>
 				</DropdownMenuItem>
 				<DropdownMenuItem onClick={() => setTheme("dark")}>
-					Dark
+					<MoonIcon className="mr-2 size-4" />
+					<span>Dark</span>
 				</DropdownMenuItem>
 				<DropdownMenuItem onClick={() => setTheme("system")}>
-					System
+					<LaptopIcon className="mr-2 size-4" />
+					<span>System</span>
 				</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	)
 }
-
-export default ModeToggle
