ce89446d62c7ad1a6306d463f8c5f5fe9103e8b5
Michael Padin <padinmichael201@gmail.com>
2024-10-30 02:55:34 +0800

    feat: add address validations

---
+++ src/validations/address.ts
@@ -0,0 +1,11 @@
+import { z } from "zod"
+
+export const addressSchema = z.object({
+	fullAddress: z.string(),
+	street: z.string().nullish(),
+	region: z.string().nullish(),
+	country: z.string().nullish(),
+	postalCode: z.string().nullish(),
+	latitude: z.number(),
+	longitude: z.number()
+})
