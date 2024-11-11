9dc1a6358fee02d38bcace71236cf860d8e75050
Michael Padin <padinmichael201@gmail.com>
2024-11-12 04:44:40 +0800

    chore: rename variable of verification document schema

--- src/validations/verification-document.ts
+++ src/validations/verification-document.ts
@@ -2,7 +2,7 @@ import { DocumentType } from "@prisma/client"
 import { z } from "zod"
 import { imageSchema } from "./image"
 
-export const documentVerificationSchema = z.object({
+export const verificationDocumentSchema = z.object({
 	type: z.nativeEnum(DocumentType),
 	image: imageSchema.refine((data) => data.url, {
 		message: "Required"
