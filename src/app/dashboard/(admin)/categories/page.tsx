6368ca61f33839ea4ff04fc048a72877b3818acc
Michael Padin <padinmichael201@gmail.com>
2024-11-17 06:41:01 +0800

    feat: add breadcrumb to categories page

--- src/app/dashboard/(admin)/categories/page.tsx
+++ src/app/dashboard/(admin)/categories/page.tsx
@@ -11,6 +11,7 @@ import { DataTableSkeleton } from "../../_components/data-table-skeleton"
 import { AddCategoryDialog } from "./_components/add-category-dialog"
 import { getCategoriesUseCase } from "@/use-cases/categories"
 import { DataTable } from "./_components/data-table"
+import { BreadcrumbResponsive } from "@/components/fg/back-button"
 
 async function getCategories() {
 	return await getCategoriesUseCase()
@@ -18,10 +19,16 @@ async function getCategories() {
 
 export default async function CategoriesPage() {
 	const categoriesPromise = getCategories()
+
+	const breadcrumbItems = [
+		{ href: "/dashboard", label: "Dashboard" },
+		{ label: "Categories" }
+	]
 	return (
 		<div>
 			<Header />
 			<div className="space-y-2 p-2 lg:space-y-4 lg:p-5">
+				<BreadcrumbResponsive items={breadcrumbItems} />
 				<Card className="">
 					<CardHeader className="p-4 lg:p-6">
 						<div className="flex items-center justify-between">
