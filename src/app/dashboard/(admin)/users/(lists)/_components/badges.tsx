c78539789a158e354e4068c6916db85154c231b4
Michael Padin <padinmichael201@gmail.com>
2024-11-21 13:49:45 +0800

    feat: add new badge ui for order status

--- src/app/dashboard/(admin)/users/(lists)/_components/badges.tsx
+++ src/app/dashboard/(admin)/users/(lists)/_components/badges.tsx
@@ -3,6 +3,7 @@ import { Badge, BadgeProps } from "@/components/ui/badge"
 import { cn } from "@/lib/utils"
 import {
 	FarmerApplicationStatus,
+	OrderStatus,
 	ProductListingStatus,
 	ROLE
 } from "@prisma/client"
@@ -140,6 +141,40 @@ export const ProductListingStatusBadge = ({
 	className,
 	showText = false
 }: ProductListingStatusBadgeProps) => {
+	const getStatusStyles = (status: ProductListingStatus) => {
+		switch (status) {
+			case "PENDING":
+				return `text-yellow-800  ${showText ? "bg-yellow-100" : "bg-yellow-200"} hover:bg-yellow-200`
+			case "APPROVED":
+				return `text-green-800 ${showText ? "bg-green-100" : "bg-green-200"} hover:bg-green-200`
+			case "REJECTED":
+				return `text-red-800 ${showText ? "bg-red-100" : "bg-red-200"} hover:bg-red-200`
+			default:
+				return "bg-gray-100 text-gray-800 hover:bg-gray-200"
+		}
+	}
+
+	return (
+		<Badge
+			variant="secondary"
+			className={cn(getStatusStyles(status), className)}
+		>
+			{showText && status.charAt(0).toUpperCase() + status.slice(1)}
+		</Badge>
+	)
+}
+
+interface OrderStatusBadgeProps {
+	status: OrderStatus
+	className?: string
+	showText?: boolean
+}
+
+export const OrderStatusBadge = ({
+	status,
+	className,
+	showText
+}: OrderStatusBadgeProps) => {
 	const getStatusStyles = (status: ProductListingStatus) => {
 		switch (status) {
 			case "PENDING":
