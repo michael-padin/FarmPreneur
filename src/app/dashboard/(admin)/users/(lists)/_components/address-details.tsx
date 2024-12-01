a01d065b8b503a0b80c9017c55a39f5aa1f0953f
Michael Padin <padinmichael201@gmail.com>
2024-12-01 16:16:56 +0800

    fix: update farmer address column

--- src/app/dashboard/(admin)/users/(lists)/_components/address-details.tsx
+++ src/app/dashboard/(admin)/users/(lists)/_components/address-details.tsx
@@ -50,7 +50,6 @@ export const AddressDetailsDrawerDialog = ({
 		return (
 			<Dialog open={open} onOpenChange={setOpen}>
 				<div>
-					{/* <p className="w-[180px] truncate">{address?.fullAddress}</p> */}
 					<DialogTrigger asChild>
 						<span className="cursor-pointer text-xs text-primary">
 							View in map
