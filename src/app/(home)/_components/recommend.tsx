a3357db93dcb9d63982f874781d3de5535e4a4d2
Michael Padin <padinmichael201@gmail.com>
2024-12-06 17:31:45 +0800

    fix: uncomment recommend products for now

--- src/app/(home)/_components/recommend.tsx
+++ src/app/(home)/_components/recommend.tsx
@@ -1,21 +1,21 @@
 import { products } from "@/data"
-import ProductCard from "./product-card"
 
 const Recommend = () => {
 	return (
 		<div>
 			<div className="grid grid-cols-2 gap-2 lg:grid-cols-5">
 				{[...products, ...products, ...products].map((product, index) => (
-					<ProductCard
-						description={product.description}
-						key={index}
-						images={product.images}
-						title={product.title}
-						price={product.price}
-						farmer={product.farmDetails}
-						unit={product.unit}
-						className="border-none shadow-none"
-					/>
+					// <ProductCard
+					// 	description={product.description}
+					// 	key={index}
+					// 	images={product.images}
+					// 	title={product.title}
+					// 	price={product.price}
+					// 	farmer={product.farmDetails}
+					// 	unit={product.unit}
+					// 	className="border-none shadow-none"
+					// />
+					<></>
 				))}
 			</div>
 		</div>
