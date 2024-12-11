9c3c497f1ec441174aa82127c95984b9bdc5b26e
Michael Padin <padinmichael201@gmail.com>
2024-12-12 00:32:02 +0800

    fix: restructure html tags

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,27 +7,27 @@ export const experimental_ppr = true
 
 export default function Home() {
 	return (
-		<div>
+		<>
+			<header>
+				<HomeNav />
+			</header>
 			<main className="bg-secondary pb-16">
-				<header>
-					<HomeNav />
-					<section className="m-auto w-full">
-						{/* <div className="absolute  inset-0  bg-black/20 backdrop-blur-sm"></div> */}
-						<div className="container relative mx-auto h-full px-0 lg:px-4 lg:pt-[101px]">
-							<div className="flex items-center justify-center bg-primary pb-20 pt-28 lg:rounded-lg lg:py-20">
-								<div className="relative space-y-5 text-center text-white lg:space-y-10">
-									<h1 className="text-3xl font-bold leading-normal tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-										Discover the Best Local Produce
-									</h1>
-									<p className="mx-auto max-w-[700px] lg:text-xl">
-										Connect directly with farmers and get the freshest,
-										highest-quality produce delivered to your door.
-									</p>
-								</div>
+				<section className="m-auto w-full">
+					{/* <div className="absolute  inset-0  bg-black/20 backdrop-blur-sm"></div> */}
+					<div className="container relative mx-auto h-full px-0 lg:px-4 lg:pt-[101px]">
+						<div className="flex items-center justify-center bg-primary pb-20 pt-28 lg:rounded-lg lg:py-20">
+							<div className="relative space-y-5 text-center text-white lg:space-y-10">
+								<h1 className="text-3xl font-bold leading-normal tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+									Discover the Best Local Produce
+								</h1>
+								<p className="mx-auto max-w-[700px] lg:text-xl">
+									Connect directly with farmers and get the freshest,
+									highest-quality produce delivered to your door.
+								</p>
 							</div>
 						</div>
-					</section>
-				</header>
+					</div>
+				</section>
 				<div className="mx-2">
 					<Categories />
 					<Features />
@@ -35,6 +35,6 @@ export default function Home() {
 					<BottomNav />
 				</div>
 			</main>
-		</div>
+		</>
 	)
 }
