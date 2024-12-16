e82a5edf3db74ad6380637d99ab18bf200211413
Michael Padin <padinmichael201@gmail.com>
2024-12-16 14:41:34 +0800

    fix: remove card-like on edit address form

--- src/app/dashboard/farmer/profile/address/[id]/edit/_components/edit-address-form..tsx
+++ src/app/dashboard/farmer/profile/address/[id]/edit/_components/edit-address-form..tsx
@@ -175,7 +175,11 @@ export function FarmerEditAddressForm({ address }: AddressFormProps) {
 						</FormItem>
 					)}
 				/> */}
-				<Button type="submit" className="w-full" disabled={isPending}>
+				<Button
+					type="submit"
+					className="w-full"
+					disabled={isPending || !form.formState.isDirty}
+				>
 					{isPending ? "Updating..." : "Update"}
 				</Button>
 			</form>
