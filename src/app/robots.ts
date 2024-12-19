f2a5c6ea44dc728ae398956d1b0b818611bf08d5
Michael Padin <padinmichael201@gmail.com>
2024-12-19 15:31:17 +0800

    feat: update robot and open graph image

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,15 @@
+import { MetadataRoute } from "next"
+
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 	? `https://${process.env.NEXT_PUBLIC_BASE_URL}`
 	: "http://localhost:3000"
 
-export default function robots() {
+export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: [
 			{
-				userAgent: "*"
+				userAgent: "*",
+				allow: "/"
 			}
 		],
 		sitemap: `${baseUrl}/sitemap.xml`,
