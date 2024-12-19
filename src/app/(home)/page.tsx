1374859e448fa1b1636fc0b431be040823547642
Michael Padin <padinmichael201@gmail.com>
2024-12-19 23:27:21 +0800

    feat: update home page metadata

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,12 +9,30 @@ import { Features } from "./_components/features"
 import { HomeNav } from "./_components/home-nav"
 import { ProductCardSkeleton } from "./_components/product-card-skeleton"
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+	? `https://${process.env.NEXT_PUBLIC_BASE_URL}`
+	: "http://localhost:3000"
 export const experimental_ppr = true
 
 export const metadata: Metadata = {
-	title: "FarmPreneur | Fresh Local Produce Marketplace",
+	title: "Fresh Local Produce Marketplace",
 	description:
-		"Discover and purchase fresh, locally-grown produce from farmers in your area. Support local agriculture with FarmPreneur."
+		"Discover and purchase fresh, locally-grown produce from farmers in your area. Support local agriculture with FarmPreneur.",
+	openGraph: {
+		type: "website",
+		url: baseUrl,
+		description:
+			"Fresh produce from local farmers, available for pickup. Support your local agriculture with FarmPreneur.",
+		siteName: "FarmPreneur",
+		images: [
+			{
+				url: `/image.png`,
+				width: 1200,
+				height: 630,
+				alt: "FarmPreneur - Local Farmers Marketplace"
+			}
+		]
+	}
 }
 
 export default function Home() {
