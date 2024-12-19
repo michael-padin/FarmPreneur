e2a07663be3d13939ed7a87d0294044210f69edb
Michael Padin <padinmichael201@gmail.com>
2024-12-19 15:40:03 +0800

    feat: update opengraph-image and breadcrumb

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,7 +1,37 @@
-import OpenGraphImage from "@/components/open-graph-image"
+import LogoIcon from "@/components/logo-icon"
+import { ImageResponse } from "next/og"
 
 export const runtime = "edge"
 
 export default async function Image() {
-	return await OpenGraphImage()
+	const { title } = {
+		...{
+			title: process.env.SITE_NAME
+		}
+	}
+
+	return new ImageResponse(
+		(
+			<div tw="flex h-full w-full flex-col items-center justify-center bg-black">
+				<div tw="flex flex-none items-center justify-center border border-neutral-700 h-[160px] w-[160px] rounded-3xl">
+					<LogoIcon width="64" height="58" fill="white" />
+				</div>
+				<p tw="mt-12 text-6xl font-bold text-white">{title}</p>
+			</div>
+		),
+		{
+			width: 1200,
+			height: 630,
+			fonts: [
+				{
+					name: "Poppins",
+					data: await fetch(
+						new URL("./fonts/Inter-Bold.ttf", import.meta.url)
+					).then((res) => res.arrayBuffer()),
+					style: "normal",
+					weight: 700
+				}
+			]
+		}
+	)
 }
