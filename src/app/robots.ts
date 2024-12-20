b304d1c4dfd8a22aca06fab6da973527ae0b5d75
Michael Padin <padinmichael201@gmail.com>
2024-12-21 05:11:29 +0800

    chore: update robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,13 +10,7 @@ export default function robots(): MetadataRoute.Robots {
 			{
 				userAgent: "*",
 				allow: "/",
-				disallow: [
-					"/dashboard/",
-					"/profile/",
-					"/orders/",
-					"/cart/",
-					"/notifications/"
-				]
+				disallow: "/private/"
 			}
 		],
 		sitemap: `${baseUrl}/sitemap.xml`
