579a12c4b5885033b0beee54c7c562f23de2c392
Michael Padin <padinmichael201@gmail.com>
2024-12-30 11:57:12 +0800

    fix: remove parent div on profile picture of farmer in home page

--- src/app/dashboard/farmer/_components/profile-picture.tsx
+++ src/app/dashboard/farmer/_components/profile-picture.tsx
@@ -6,17 +6,19 @@ export async function ProfilePicture() {
 	const user = (await auth())?.user
 
 	return (
-		<div className="relative h-20 w-20 overflow-hidden rounded-full">
-			<Link href={`/dashboard/farmer/profile`} prefetch>
-				<Image
-					src={user?.profilePicture || "/placeholder.svg"}
-					alt={`Farmer's profile picture`}
-					fill
-					priority
-					sizes="(min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
-					className="rounded-full object-cover"
-				/>
-			</Link>
-		</div>
+		<Link
+			href={`/dashboard/farmer/profile`}
+			prefetch
+			className="relative h-20 w-20 overflow-hidden rounded-full"
+		>
+			<Image
+				src={user?.profilePicture || "/placeholder.svg"}
+				alt={`Farmer's profile picture`}
+				fill
+				priority
+				sizes="(min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+				className="rounded-full object-cover"
+			/>
+		</Link>
 	)
 }
