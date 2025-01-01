e322d2a187c3a9a0dada28f9c91d00ae75ad63bc
Michael Padin <padinmichael201@gmail.com>
2025-01-01 17:03:25 +0800

    feat: update service worker file

--- public/sw.js
+++ public/sw.js
@@ -20,7 +20,6 @@ self.addEventListener("push", function (event) {
 })
 
 self.addEventListener("notificationclick", function (event) {
-	console.log("Notification click received.")
 	event.notification.close()
-	event.waitUntil(clients.openWindow(baseUrl))
+	event.waitUntil(clients.openWindow(event.notification.data?.url))
 })
