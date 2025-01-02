c21789cbed4cfbaca53510627907b3f6be910cf3
Michael Padin <padinmichael201@gmail.com>
2025-01-03 04:55:13 +0800

    feat: update push service worker

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,7 @@ self.addEventListener("push", function (event) {
 			badge: "/badge.png",
 			vibrate: [100, 50, 100],
 			data: {
-				url: JSON.parse(event.data).url,
+				url: data.url,
 				dateOfArrival: Date.now(),
 				primaryKey: "2"
 			}
